c40a166e7e7289491b8af5eb46b17253be2ef31e
rezwan <rezwan@Xubuntu.myguest.virtualbox.org>
2024-08-24 20:00:02 -0400

    Add delete buttons and toggle buttons

--- main.js
+++ main.js
@@ -32,36 +32,94 @@ function displayBooks() {
     });
 }
 
+
+
 // Creates a row entry for each book
+let index = 0;
 function createBookEntry(book) {
     const tableRow = document.createElement("tr");
-    
+    tableRow.setAttribute("data-index", index)
+    const deleteButtonCell = createDeleteButton();
+    tableRow.appendChild(deleteButtonCell);
 
     for (let key in book) {
+
         const rowCell = createDataCellFromBook(key, book[key]);
         tableRow.appendChild(rowCell);
     }
+    const toggleButtonCell = createToggleButton(book);
+    tableRow.appendChild(toggleButtonCell);
     return tableRow;
+}
+
+
+// Create a delete button for each book
+function createDeleteButton() {
+    const cell = document.createElement("td");
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("del-button");
+    
+    index++;
+    deleteButton.innerHTML = `
+    <svg class="trashcan" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <title>Remove</title>
+    <path
+      d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+  </svg>`;
+
+    deleteButton.addEventListener("click", deleteBook);
+    cell.appendChild(deleteButton);
+    cell.classList.add("transparent")
+    return cell;
+}
+
+function deleteBook(event) {
+    let bookIndex = event.target.closest("tr").getAttribute("data-index");
+    myLibrary.splice(bookIndex, 1);
+    document.querySelector('tbody').innerHTML = '';
+    index = 0;
+    displayBooks();
 }
 
 function createDataCellFromBook(key, value) {
     const cellData = document.createElement("td");
     if (key === "isBookRead") {
+        cellData.classList.add("read-cell")
         cellData.innerHTML = bookCompletionStatus(value, cellData);
-
     } else {
         cellData.textContent = value;
     }
     return cellData;
 }
 
+// For the toggle button
+function createToggleButton(book) {
+    const cell = document.createElement("td");
+    const button = document.createElement("button");
+    
+    button.innerHTML = bookCompletionStatus(!book.isBookRead, button);
+    button.addEventListener("click", () => {
+        button.innerHTML = bookCompletionStatus(book.isBookRead, button);
+        book.isBookRead = !book.isBookRead;
+        const bookEntry = button.closest("[data-index]")
+        const bookReadCell = bookEntry.querySelector(".read-cell");
+        bookReadCell.innerHTML = bookCompletionStatus(book.isBookRead, bookReadCell);
+    });
+
+    cell.appendChild(button);
+    cell.classList.add("transparent");
+    return cell;
+}
+
+
 // I want to use check / corss mark to represnt wheater or not a book was read
-function bookCompletionStatus(bool, cell) {
+function bookCompletionStatus(bool, element) {
+    element.classList.remove("green-text", "red-text");
     if (bool) {
-        cell.classList.add("green-text");
+        element.classList.add("green-text");
         return "&#10004;";
     } else {
-        cell.classList.add("red-text");
+        element.classList.add("red-text");
         return "&#10005;";
     }
 }
@@ -89,7 +147,7 @@ function getFormData(event) {
     const formData = new FormData(event.target);
     const data = validateFormData(formData);
     const newBook = new Book(data.title, data.author, +data.pages,
-         data["completion-status"]);
+        data["completion-status"]);
     addBookToLibrary(newBook);
     displayNewBook(newBook);
     event.target.reset();
@@ -98,12 +156,12 @@ function getFormData(event) {
 function validateFormData(formData) {
     const data = {};
     formData.forEach((value, key) => {
-        if(key === "completion-status") {
+        if (key === "completion-status") {
             data[key] = value === "read" ? true : false;
         } else {
             data[key] = value;
         }
-        
+
     });
     return data;
 }
